9074c720e2af854a1514583be9081159361c57af
Mawutoragbavor <mawutoragbavor@ymail.com>
2022-10-19 17:55:16 -0700

    printf in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-/*
+/**
  * main - Print a line of code using puts
- * Return: Always 0 (Successful)
- */
+ *
+ * Return: 0
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
