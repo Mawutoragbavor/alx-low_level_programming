25d092ebdb7f6a9826fe3fa07ce8a594336c69d4
Mawutoragbavor <mawutoragbavor@ymail.com>
2022-10-25 08:51:12 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * main - print _putchar
- * is written by Mak
- * Return: 0
+ * main-main code
+ * Description:print -putchar in a new line
+ * Return:this program returns zero
  */
-
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');	
+	_putchar('r');
+	_putchar('\n');
+											return (0);
 }
